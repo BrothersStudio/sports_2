58d32743c23031efa737d1b759f8eb77d3d8a763
Patrick <pwcarney@bu.edu>
2020-04-01 22:07:35 -0400

    Flying into things destroys them

---
+++ Assets/Scripts/WreckingBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WreckingBall : MonoBehaviour
+{
+    public bool dangerous = false;
+    public Color dangerous_color;
+    public Color safe_color;
+
+    public void Fired()
+    {
+        dangerous = true;
+        Invoke("Cooldown", 0.5f);
+
+        GetComponent<SpriteRenderer>().color = dangerous_color;
+    }
+
+    void Cooldown()
+    {
+        dangerous = false;
+
+        GetComponent<SpriteRenderer>().color = safe_color;
+    }
+}
