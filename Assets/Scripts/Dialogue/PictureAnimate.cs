b91e5621b4450b714eb3cd783265b76df31c80b5
Patrick <pwcarney@bu.edu>
2020-04-28 22:58:53 -0400

    Added new captian body and head

--- Assets/Scripts/Dialogue/PictureAnimate.cs
+++ Assets/Scripts/Dialogue/PictureAnimate.cs
@@ -8,35 +8,44 @@ public class PictureAnimate : MonoBehaviour
     private TMP_Animated animatedText;
 
     private bool moved = false;
-    private float move_amount = 0.5f;
+    private float move_amount = 10f;
     private Vector3 default_pos;
 
-    void Start()
+    void Awake()
     {
-        default_pos = transform.position;
+        default_pos = GetComponent<RectTransform>().localPosition;
 
-        animatedText = transform.parent.Find("Text Box/Text (TMP)").GetComponent<TMP_Animated>();
+        animatedText = transform.parent.parent.Find("Text Box/Text (TMP)").GetComponent<TMP_Animated>();
         animatedText.onTextReveal.AddListener((newChar) => AnimatePicture());
+        animatedText.onDialogueFinish.AddListener(() => DoneCurrentText());
     }
 
     public void AnimatePicture()
     {
         if (moved)
         {
-            transform.position = default_pos;
+            GetComponent<RectTransform>().localPosition = default_pos;
             moved = false;
         }
         else
         {
-            transform.position += new Vector3(
-                Random.Range(0, 0),
-                Random.Range(-move_amount, move_amount));
+            GetComponent<RectTransform>().localPosition += new Vector3(
+                Random.Range(-move_amount, move_amount),
+                Random.Range(-move_amount, move_amount),
+                0);
             moved = true;
         }
     }
 
+    private void DoneCurrentText()
+    {
+        GetComponent<RectTransform>().localPosition = default_pos;
+        moved = false;
+    }
+
     private void OnDisable()
     {
-        transform.position = default_pos;
+        GetComponent<RectTransform>().localPosition = default_pos;
+        moved = false;
     }
 }
