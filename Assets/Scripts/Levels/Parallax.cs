c8d90e8959c02a431a2a2153fc89bd700fc91109
Patrick <pwcarney@bu.edu>
2020-04-29 21:12:11 -0400

    Added parallax effect to background

---
+++ Assets/Scripts/Levels/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Transform main_camera;
+    private Vector3 last_pos;
+    private float parallax_percent = 20;
+
+    private void Awake()
+    {
+        main_camera = Camera.main.transform;
+        last_pos = main_camera.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 diff = main_camera.position - last_pos;
+        diff *= parallax_percent / 100f;
+        transform.position += diff;
+        last_pos = main_camera.position;
+    }
+}
